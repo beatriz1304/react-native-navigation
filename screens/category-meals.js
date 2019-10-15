80457667d47154291cb5c64ab99ef212efc232c0
Beatriz da Silveira <beatriz1304@gmail.com>
2019-10-15 20:00:11 -0300

    Add dummy content to navigating between these created screens

---
+++ screens/category-meals.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const CategoryMeal = props => {
+  return (
+    <View styles={styles.screen}>
+      <Text>The Category Meal Screen!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+const styles = StyleSheet.create({})
+
+export default CategoryMeal
---
+++ screens/filters.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Filters = props => {
+  return (
+    <View styles={styles.screen}>
+      <Text>The Filters Screen!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+const styles = StyleSheet.create({})
+
+export default Filters
