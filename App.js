84d5f631c2ebe585a689b6f770647af0436124d1
Beatriz da Silveira <beatriz1304@gmail.com>
2019-10-20 20:14:33 -0300

    Add main page

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  )
 }
