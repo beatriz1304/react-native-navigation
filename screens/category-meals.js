d00f35c291c60afb0ad7ea870a16667b233e4fb4
Beatriz da Silveira <beatriz1304@gmail.com>
2019-10-25 20:37:03 -0300

    Navigate from category meals to category detail

--- screens/category-meals.js
+++ screens/category-meals.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
 const CategoryMeal = props => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Button
+        title='Go to Details'
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' })
+        }}
+      />
     </View>
   )
 }
