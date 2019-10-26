e193c616c6b7da3582b641f97a9d41b0393cbd58
Beatriz da Silveira <beatriz1304@gmail.com>
2019-10-25 22:22:04 -0300

    Set a dynamic title navigation

--- screens/category-meals.js
+++ screens/category-meals.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
+import { CATEGORIES } from '../data/categories'
+
 const CategoryMeal = props => {
+  const categoryId = props.navigation.getParam('categoryId')
+  const selectedCategory = CATEGORIES.find(item => item.id === categoryId)
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title='Go to Details'
         onPress={() => {
           props.navigation.navigate({ routeName: 'MealDetail' })
         }}
       />
+
+      <Button
+        title='Go Back'
+        onPress={() => {
+          props.navigation.pop()
+        }}
+      />
     </View>
   )
+}
+
+CategoryMeal.navigationOptions = navigationData => {
+  const categoryId = navigationData.navigation.getParam('categoryId')
+  const selectedCategory = CATEGORIES.find(item => item.id === categoryId)
+
+  return { headerTitle: selectedCategory.title }
 }
 
 const styles = StyleSheet.create({
